f78e46ff5d5486adfbbdd49c05d9ce0ef76488b2
H-strangeone <hiteshgitman@gmail.com>
2024-01-31 23:01:20 -0800

    i

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let guess:u32 = "10".parse().expect("naah");
+    let guess2: String= 43.to_string();
+    println!("guess is {}",guess);
+    println!("gues2 is {}", guess2);
+    println!(typeof)
+}
